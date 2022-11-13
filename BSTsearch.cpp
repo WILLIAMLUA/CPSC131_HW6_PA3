5774802c3f3c95ea153bdf82f57332a3476c91d0
WILLIAMLUA <98416548+WILLIAMLUA@users.noreply.github.com>
2022-11-12 21:32:10 -0800

    Update BSTsearch.cpp

--- BSTsearch.cpp
+++ BSTsearch.cpp
@@ -1,30 +1,36 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ * PROBLEM 1: SEARCH IN A BINARY SEARCH TREE
+ * WILLIAM LUA
+ * MARIAH SALGADO
  */
-class Solution {
+
+class Solution { //Struct TreeNode
 public:
-    TreeNode* searchBST(TreeNode* root, int val) {
-        TreeNode* temp = root;
+    TreeNode* searchBST(TreeNode* root, int val) { //int Val;
+        TreeNode* temp = root; 
         while(temp != NULL){
             if(temp->val == val){
                 return temp;
             }
             else if(val < temp->val){
-                temp = temp->left;
+                temp = temp->left; //TreeNode *left;
             }
             else{
-                temp = temp->right;
+                temp = temp->right; //TreeNode *right;
             }
         }
         return NULL;
     }
 };
    
+/**
+*Solved with recursion
+*struct TreeNode {
+*int val;
+*TreeNode *left;
+*TreeNode *right;
+*TreeNode() : val(0), left(nullptr), right(nullptr) {}
+*TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+*TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+* };
+*/
